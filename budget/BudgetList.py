6f3a6b8208cc1b9fae3d27e1aff552f284fd4fae
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-06 23:45:16 -0600

    Moudule #3

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 
 
 class BudgetList:
@@ -21,6 +22,17 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return next(self.iter_e)
+        except StopIteration:
+            return next(self.iter_o)
+
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -29,6 +41,14 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
     print('The count of all expenses: ' + str(len(myBudgetList)))
+    for entry in myBudgetList:
+        print(entry)
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
 
 if __name__ == "__main__":
