4897d42a959cb9144858e23fb010bb25f69889ef
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-06 23:26:12 -0600

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -5,9 +5,9 @@ class BudgetList:
 
     def __init__(self, budget):
         self.budget = budget
-        self.sum_expenses = float(0)
+        self.sum_expenses = 0
         self.expenses = []
-        self.sum_overages = float(0)
+        self.sum_overages = 0
         self.overages = []
 
     def append(self, item):
