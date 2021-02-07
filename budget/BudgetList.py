53fd606a0e7a4bb1f5e2d90dd8738d12909fc5e9
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-06 23:28:47 -0600

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -28,7 +28,7 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses: {}'.format(str(myBudgetList.__len__())))
+    print('The count of all expenses: {}'.format(str(len(myBudgetList))))
 
 
 if __name__ == "__main__":
