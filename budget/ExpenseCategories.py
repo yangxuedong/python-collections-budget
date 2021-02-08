ecd553b4126a504874ec9b713205aa437342be44
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-07 22:00:01 -0600

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -13,18 +13,16 @@ def main():
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subset test')
-    """
-    print(timeit.timeit(stmt='expenses.categorize_for_loop()',
-                  setup='...'\
-                        'from . import Expense'\
-                        'expenses = Expense.Expenses()'\
-                        'expenses.read_expenses('data/spending_data.csv')'\
-                        '...',
-                  number=100000,
-                  globals=globals()
-                  )
-    )
-    """
+
+        print(timeit.timeit(stmt="expenses.categorize_for_loop()",
+                            setup=
+                            '''
+    from . import Expense
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+                            ''',
+                            number=100000,
+                            globals=globals()))
 
 
 if __name__ == "__main__":
